caca1de0f14bd547f702651f16e35762635c0edc
Panuwat Churee <ben.panuwat.c@gmail.com>
2021-01-18 13:10:21 +0700

    update 1

--- src/app/profile/my-account/my-account.component.ts
+++ src/app/profile/my-account/my-account.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AppService } from 'src/app/app.service';
 
 @Component({
   selector: 'app-my-account',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyAccountComponent implements OnInit {
 
-  constructor() { }
+  public headers: HttpHeaders;
+
+  public member = {
+    id: ""
+  };
+
+  constructor(private http: HttpClient, private service: AppService) {
+  }
 
   ngOnInit(): void {
+    this.headers = new HttpHeaders();
+    this.headers = this.headers.append('Authorization', localStorage.getItem('token'));
+
+    this.member.id = localStorage.getItem('id');
+
+    this.http.post<any>(this.service.url + '/api/get_accunt', this.member, { headers: this.headers }).subscribe(res => {
+      if (res.success) {
+        this.member = res.data.member;
+      }
+    });
   }
 
 }
